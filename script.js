793b74e46fd271b2a559e7dda6671030f807777c
aboxnz <alan@abox.co.nz>
2016-06-01 20:40:26 +1200

    Basic file struction and starting divs / button

---
+++ script.js
@@ -0,0 +1,7 @@
+var divGrid = document.createElement("div");
+divGrid.className = "grid";
+var container = document.getElementById("container");
+
+function generate() {
+  container.appendChild(divGrid);
+}
