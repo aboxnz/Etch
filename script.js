b899ae12189d991d0872c4a31221f074b485244c
aboxnz <alan@abox.co.nz>
2016-06-03 07:00:06 +1200

    Basic proof
    Need to change from append number of squares to append rows and columns

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-var divGrid = document.createElement("div");
-divGrid.className = "grid";
-var container = document.getElementById("container");
 
 function generate() {
-  container.appendChild(divGrid);
+  var gridSize = parseInt(document.getElementById("gridSize").value);
+
+  for (i = 0; i <= gridSize; i++) {
+  	var mainContainer = document.getElementById("container");
+    var divGrid = document.createElement("div");
+    divGrid.className = "grid";
+  	mainContainer.appendChild(divGrid);
+  }
+
+  $('.grid').mouseenter(function(){
+  	$(this).addClass("black");
+  });
+  $('.clearButton').click(function() {
+  	$("div").remove('.grid');
+  })  
 }
