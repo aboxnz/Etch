307dfac59511370c6c8056be6e3195695096c6f4
aboxnz <alan@abox.co.nz>
2016-06-03 22:41:28 +1200

    Complete basic setup
    Including some changed styling & proper grid generation.

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 
 function generate() {
-  var gridSize = parseInt(document.getElementById("gridSize").value);
+  var squares = prompt("Number of squares? ( 0 < X < 120 )")
+  var mainContainer = document.getElementById("container");
+  var width = 960;
+  var size = width / squares;
 
-  for (i = 0; i <= gridSize; i++) {
-  	var mainContainer = document.getElementById("container");
-    var divGrid = document.createElement("div");
-    divGrid.className = "grid";
-  	mainContainer.appendChild(divGrid);
-  }
 
-  $('.grid').mouseenter(function(){
-  	$(this).addClass("black");
+
+  for (i = 0; i < squares; i++) {
+  	$('#container').append("<div class='row'>");
+  	for (j = 0; j < squares; j++) {
+	  $('#container').append("<div class='square'></div>");
+  	};
+  	$('#container').append("</div>");
+  };
+
+  $('.square').css("width", size);
+  $('.square').css("height", size);
+
+
+  $('.square').hover(function(){
+    $(this).addClass("black");
   });
   $('.clearButton').click(function() {
-  	$("div").remove('.grid');
-  })  
+    $("div").remove('.square');
+  })
 }
