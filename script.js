cfac8898376fe5cf1ff782bd460c2a1c83c70f1b
aboxnz <alan@abox.co.nz>
2016-06-06 14:58:23 +1200

    Large amout of changes
    Most notable is addition of color randomizer via RGB string

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
-
 function generate() {
-  var squares = prompt("Number of squares? ( 0 < X < 120 )")
-  var mainContainer = document.getElementById("container");
-  var width = 960;
-  var size = width / squares;
+var squares = prompt("Number of squares? ( 0 < X < 40 )")
+var width = 960;
+var size = width / squares;
 
+if (squares == 0) {
+	alert("Can't make 0 squares!");
+} else if (squares > 40) {
+	alert("You have too many squares - setting to 40 squares");
+	squares = 40;
+};
 
+for (i = 0; i < squares; i++) {
+  $('#container').append("<div class='row'>");
+    for (j = 0; j < squares; j++) {
+      $('#container').append("<div class='square'></div>");
+	};
+  $('#container').append("</div>");
+};
 
-  for (i = 0; i < squares; i++) {
-  	$('#container').append("<div class='row'>");
-  	for (j = 0; j < squares; j++) {
-	  $('#container').append("<div class='square'></div>");
-  	};
-  	$('#container').append("</div>");
-  };
+$('.square').css("width", size);
+$('.square').css("height", size);
 
-  $('.square').css("width", size);
-  $('.square').css("height", size);
+};
 
-
+function defaultMouse() {
   $('.square').hover(function(){
-    $(this).addClass("black");
+    $(this).addClass('change');
   });
-  $('.clearButton').click(function() {
-    $("div").remove('.square');
-  })
-}
+};
+
+function trailMouse() {
+  $('.square').mouseenter(function(){
+  	$(this).fadeTo('fast', 0.1);
+  });
+  $('.square').mouseleave(function(){
+  	$(this).fadeTo('fast', 1);
+  });
+};
+
+function fadeMouse() {
+  $('.square').on('mouseenter', function() {
+  var opacity = $(this).css('opacity');
+  $(this).css('opacity', opacity - 0.1);
+});
+};
+
+function randomColour() {
+  $('.square').mouseenter(function() {
+    $(this).css('background-color', randomizer());
+  });
+};
+
+function randomizer() {
+  var r = parseInt(Math.random() * 255);
+  var g = parseInt(Math.random() * 255);
+  var b = parseInt(Math.random() * 255);
+  var colourPicker = "rgb" + "(" + r + "," + g + "," + b + ")";
+  return colourPicker
+};
+
