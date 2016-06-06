d07802ef85857a88fe89ef7ffe30a88ee2371714
aboxnz <alan@abox.co.nz>
2016-06-06 22:42:58 +1200

    CSS Styles

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ $(document).on('click','.clearButton', function() {
 function generate() {
 var squares = prompt("Number of squares? ( 0 < X < 40 )")
 var width = 960;
-var size = width / squares;
+
 
 if (squares == 0) {
 	alert("Can't make 0 squares!");
 } else if (squares > 40) {
-	alert("You have too many squares - setting to 40 squares");
+	alert("Too many squares - setting to 40 squares");
 	squares = 40;
 };
+
+var size = width / squares;
 
 for (i = 0; i < squares; i++) {
   $('#container').append("<div class='row'>");
@@ -47,7 +49,7 @@ function trailMouse() {
 function fadeMouse() {
   $('.square').on('mouseenter', function() {
   var opacity = $(this).css('opacity');
-  $(this).css('opacity', opacity - 0.1);
+  $(this).css('opacity', opacity - 0.2);
 });
 };
 
